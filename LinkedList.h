46917aa8ec44238d24eb7952889d5b7ed988961d
jakefull <122409716+jakefull@users.noreply.github.com>
2023-02-09 15:51:06 -0700

    Made some more progress.

--- LinkedList.h
+++ LinkedList.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include <string>
-
+#include <sstream>
 #include "LinkedListInterface.h"
 
 template<typename T>
 class LinkedList: public LinkedListInterface<T> {
     class Node {
     public:
-        Node(T value) : value(value) next(nullptr) {} //constructor
+        Node(T value) : value(value), next(nullptr) {} //constructor
         T value;
         Node * next;
     };
@@ -22,29 +22,39 @@ public:
         lsize = 0;
     }
 
-    virtual ~LinkedList() {
-        // implement your destructor here
+    virtual ~LinkedList() {// we just need to delete every node in the list, yeah?
+        if(lsize!=0){
+            Node* deleteThis = head;
+            while(deleteThis->next!= nullptr){
+                delete deleteThis; //delete needs a pointer! Delete just says ok, whatever memory this pointer points to can now be used for something else. It doesnt delete the pointer itself.
+                deleteThis = deleteThis->next;
+            }
+            delete deleteThis;
+        }
     }
 
-    virtual void push_front(T item) {
-        Node addThis = new Node(item);
+    virtual void push_front(T item) { //should be implemented
+        // WHY cant we do this? Node* addThis = new Node(item); NO! Use variables already defined
         if(lsize==0){ //if empty
-            head = &addThis;
-            tail = &addThis;
+            head = new Node(item); //NOTE: new acts as the * in this case
+            tail = head;
         } else { //not empty
-            addThis.next = head;
-            head = &addThis;
+            //don't we need a temporary variable here? NO, we don't! Redo this to make it more efficient!
+            Node* oldHead = head; //NOTE: pointers don't "shallow copy" or "Deep copy." bc address. You are just assigning an address here.
+            head = new Node(item);
+            head->next = oldHead;  //SIMPLIFY (see above)
         }
         lsize++;
     }
 
-    virtual void push_back(T item) {
-        Node addThis = new Node(item);
+    virtual void push_back(T item) { //should be implemented
+        //Node addThis = new Node(item); NO! Don't do this. Use variables you already have to implement it
         if(lsize==0) { //if empty
-            head = &addThis;
-            tail = &addThis;
+            head = new Node(item); //no ampersand?
+            tail = head;
         } else { //not empty
-            tail = &addThis;
+            tail->next = new Node(item);
+            tail = tail->next;
             //x.next is already nullptr
         }
         lsize++;
@@ -68,22 +78,31 @@ public:
 
     virtual T front() const {
         // implement front here
+        T something;
+        return something;
+         //delete this later
     }
 
     virtual T back() const {
         // implement back here
+        T something;
+        return something; //delete this later
     }
 
     virtual T at(size_t index) const {
         // implement at here
+        T something;
+        return something; //delete this later
     }
 
     virtual bool contains(const T& item) const {
         // implement contains here
+        return false; //delete this later
     }
 
     virtual size_t size() const {
         // implement size here
+        return 0; //delete this later
     }
 
     virtual void clear() {
@@ -92,5 +111,13 @@ public:
 
     virtual std::string toString() const {
         // implement toString here
+        Node currentNode = *head;
+        std::stringstream ss;
+        while(currentNode.next != nullptr){
+            ss << currentNode.value << " ";
+            currentNode = *currentNode.next;
+        }
+        ss << currentNode.value;
+        return ss.str();
     }
 };
--- scratch.cpp
+++ scratch.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
 #include "LinkedList.h"
-
+using std::cout;
+using std::endl;
 int main(int argc, char* argv[]) {
     // Put any code that you want to run to test your linked list implementation here
+    LinkedList<int> intList; //something wrong with the destructor!
+    intList.push_back(3);
+    intList.push_front(2);
+    intList.push_front(1);
+    cout << intList.toString() << endl;
+    return 0;
 }
